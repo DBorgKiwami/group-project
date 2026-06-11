0b89ea7e0438e4811d10cee74a699094f973106e
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-06-11 15:55:31 +0100

    Coyote Time and variable jump height
    Added coyote time (google it if you don't know what it is) and made it so jump height varies depending on how long the spacebar is pressed.

--- scripts/playermovement/player3d.gd
+++ scripts/playermovement/player3d.gd
@@ -1,19 +1,35 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+var can_jump = false
+var jump_timer = 0.2
+
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		jump_timer -= delta;
+	
+	if is_on_floor():
+		can_jump = true;
+		jump_timer = 0.2
+	
+	if jump_timer < 0:
+		can_jump = false;
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and can_jump:
 		velocity.y = JUMP_VELOCITY
-
+		can_jump = false
+	if Input.is_action_just_released("ui_accept"):
+		velocity.y = minf(0, velocity.y)
+	
+	print(velocity.y)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
