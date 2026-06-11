5febbcd16d6202abdbcd351d9eeaa0d70387029a
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-06-11 15:58:02 +0100

    Code Comments

--- scripts/playermovement/player3d.gd
+++ scripts/playermovement/player3d.gd
@@ -12,23 +12,26 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		#Decrease jump timer whilst not on the floor
 		jump_timer -= delta;
 	
+	#If you're on the floor, you can jump
 	if is_on_floor():
 		can_jump = true;
 		jump_timer = 0.2
 	
+	#If the jump timer has run out, you cannot jump
 	if jump_timer < 0:
 		can_jump = false;
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and can_jump:
 		velocity.y = JUMP_VELOCITY
+		#If you've just pressed the jump button, you cannot jump
 		can_jump = false
 	if Input.is_action_just_released("ui_accept"):
+		#If you've let go of the jump button, choose the lower number between 0 and player velocity and set velocity to that number (If you just set it to 0, you can basically spam spacebar to cancel the effect of gravity)
 		velocity.y = minf(0, velocity.y)
-	
-	print(velocity.y)
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
