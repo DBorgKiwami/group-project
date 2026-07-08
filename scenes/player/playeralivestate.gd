46b117f9e35383d542d0f0ba313c2a0289dffda4
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-07-08 21:18:27 +0100

    Moving 2D Player code to state machine
    I swear this will make things easier later

---
+++ scenes/player/playeralivestate.gd
@@ -0,0 +1,83 @@
+extends State
+
+@export var player_reference : CharacterBody3D
+
+func update(_delta: float):
+	if player_reference.player_camera:
+		var x_diff = player_reference.position.x - player_reference.player_camera.global_position.x 
+		if abs(x_diff) > player_reference.camera_x_bound:
+			if x_diff>0:
+				player_reference.player_camera.global_position.x += x_diff - player_reference.camera_x_bound
+			else:
+				player_reference.player_camera.global_position.x += x_diff + player_reference.camera_x_bound
+		player_reference.player_camera.global_position.y = player_reference.position.y + player_reference.camera_y_offset
+
+func physicsUpdate(delta: float):
+	#Reduce the jump buffer if its still running
+	if player_reference.jump_buffer > 0:
+		player_reference.jump_buffer -= delta
+	
+	# Add the gravity.
+	if not player_reference.is_on_floor():
+		player_reference.velocity += player_reference.get_gravity() * delta
+		#Decrease jump timer whilst not on the floor
+		player_reference.jump_timer -= delta;
+	
+	#If you're on the floor, you can jump
+	if player_reference.is_on_floor():
+		player_reference.can_jump = true;
+		player_reference.jump_timer = player_reference.jump_timer_max
+	
+	#If the jump timer has run out, you cannot jump
+	if player_reference.jump_timer < 0:
+		player_reference.can_jump = false;
+	
+	# Handle jump.
+	if Input.is_action_pressed("ui_accept"):
+		#Reset the jump buffer
+		player_reference.jump_buffer = player_reference.jump_buffer_len
+	#If the jump buffer is higher than 0, the player is trying to jump
+	if player_reference.jump_buffer > 0 and player_reference.can_jump:
+		player_reference.jump_buffer = 0
+		player_reference.velocity.y = player_reference.JUMP_VELOCITY
+		#If you've just pressed the jump button, you cannot jump
+		player_reference.can_jump = false
+	if Input.is_action_just_released("ui_accept"):
+		#If you've let go of the jump button, choose the lower number between 0 and player velocity and set velocity to that number (If you just set it to 0, you can basically spam spacebar to cancel the effect of gravity)
+		player_reference.velocity.y = minf(0, player_reference.velocity.y)
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		#Flip character depending on their direction
+		if direction < 0:
+			player_reference.hitboxFront.rotation_degrees = Vector3(0, 180, 0)
+			player_reference.sprite.flip_h = true;
+		else:
+			player_reference.hitboxFront.rotation_degrees = Vector3(0, 0, 0)
+			player_reference.sprite.flip_h = false;
+		player_reference.velocity.x = direction * player_reference.SPEED
+	else:
+		player_reference.velocity.x = move_toward(player_reference.velocity.x, 0, player_reference.SPEED)
+
+func enter():
+	pass
+
+func exit():
+	pass
+
+func input(event: InputEvent):
+	if event.is_action_pressed("attack") and Input.is_action_pressed("ui_down"):
+		print("Down")
+		player_reference.animationController.play("attack_down")
+		return
+	if event.is_action_pressed("attack") and Input.is_action_pressed("ui_up"):
+		print("Down")
+		player_reference.animationController.play("attack_up")
+		return
+	if event.is_action_pressed("attack"):
+		print("Attacking")
+		#Using Godot's animation player, we can program the frames of the attack from the editor instead of purely in code!
+		player_reference.animationController.play("attack")
--- scripts/playermovement/player2d.gd
+++ scripts/playermovement/player2d.gd
@@ -6,8 +6,8 @@ extends CharacterBody3D
 @export var animationController : AnimationPlayer
 @export var player_hitbox : Area3D
 @export var player_camera : Camera3D
-
 @export var sprite : AnimatedSprite3D
+@export var state_machine : State_Machine
 
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
@@ -17,7 +17,7 @@ extends CharacterBody3D
 @export var jump_timer_max = 0.2
 @export var jump_buffer_len = 0.12
 @export var damage = 10
-@export var max_health = 5
+@export var max_health = 3
 var health
 var can_jump = false
 var jump_timer = jump_timer_max
@@ -31,93 +31,18 @@ func _ready() -> void:
 func _on_player_hit(damage) -> void:
 	print("I've been hit for " + str(damage) + "!")
 	health = health - damage
-	if health >= 0:
+	if health <= 0:
 		print("I'm fuckin dead!")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack") and Input.is_action_pressed("ui_down"):
-		print("Down")
-		animationController.play("attack_down")
-		return
-	if event.is_action_pressed("attack") and Input.is_action_pressed("ui_up"):
-		print("Down")
-		animationController.play("attack_up")
-		return
-	if event.is_action_pressed("attack"):
-		print("Attacking")
-		#Using Godot's animation player, we can program the frames of the attack from the editor instead of purely in code!
-		animationController.play("attack")
 	if event.is_action_pressed("debug_swap_level"):
 		Scenecontroler.load_scene_with_position("res://scenes/levels/3d/testlevel.tscn", Vector3(1,1,1))
 	pass
 
 func _process(delta: float) -> void:
-	#camera movement
-	if player_camera:
-		var x_diff = position.x - player_camera.global_position.x 
-		if abs(x_diff) > camera_x_bound:
-			if x_diff>0:
-				player_camera.global_position.x += x_diff - camera_x_bound
-			else:
-				player_camera.global_position.x += x_diff + camera_x_bound
-		player_camera.global_position.y = position.y + camera_y_offset
-		#var y_diff = position.y - (player_camera.global_position.y - camera_y_offset)
-		#if abs(y_diff) > camera_y_bound:
-			#if y_diff>0:
-				#player_camera.global_position.y += (y_diff - camera_y_bound)
-			#else:
-				#player_camera.global_position.y += (y_diff + camera_y_bound)
+	pass
 
 func _physics_process(delta: float) -> void:
-	#Reduce the jump buffer if its still running
-	if jump_buffer > 0:
-		jump_buffer -= delta
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		#Decrease jump timer whilst not on the floor
-		jump_timer -= delta;
-	
-	#If you're on the floor, you can jump
-	if is_on_floor():
-		can_jump = true;
-		jump_timer = jump_timer_max
-	
-	#If the jump timer has run out, you cannot jump
-	if jump_timer < 0:
-		can_jump = false;
-	
-	# Handle jump.
-	if Input.is_action_pressed("ui_accept"):
-		#Reset the jump buffer
-		jump_buffer = jump_buffer_len
-	#If the jump buffer is higher than 0, the player is trying to jump
-	if jump_buffer > 0 and can_jump:
-		jump_buffer = 0
-		velocity.y = JUMP_VELOCITY
-		#If you've just pressed the jump button, you cannot jump
-		can_jump = false
-	if Input.is_action_just_released("ui_accept"):
-		#If you've let go of the jump button, choose the lower number between 0 and player velocity and set velocity to that number (If you just set it to 0, you can basically spam spacebar to cancel the effect of gravity)
-		velocity.y = minf(0, velocity.y)
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	
-	if direction:
-		#Flip character depending on their direction
-		if direction < 0:
-			hitboxFront.rotation_degrees = Vector3(0, 180, 0)
-			sprite.flip_h = true;
-		else:
-			hitboxFront.rotation_degrees = Vector3(0, 0, 0)
-			sprite.flip_h = false;
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
 
