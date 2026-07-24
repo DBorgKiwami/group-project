38755bdac183bb15c8c3d7dec65797567a7cbae2
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-07-24 17:51:11 +0100

    Multiple dropped coins

--- scripts/enemies/2dtestenemy.gd
+++ scripts/enemies/2dtestenemy.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 @export var animationControler : AnimationPlayer
 @export var damage : int = 1
 @export var drop : PackedScene
+@export var dropamount : int = 1
 
 func _ready():
 	hitbox.connect("on_hit", hitbox_hit)
@@ -14,9 +15,10 @@ func die():
 	animationControler.play("die")
 	await animationControler.animation_finished
 	if drop:
-		var newInstance = drop.instantiate()
-		newInstance.position = position
-		get_tree().root.add_child(newInstance)
+		for i in dropamount:
+			var newInstance = drop.instantiate()
+			newInstance.position = position
+			get_tree().root.add_child(newInstance)
 	call_deferred("queue_free")
 
 func _physics_process(delta: float) -> void:
