aac04362a7479f46f5274b9faadbc5448e4ed421
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-07-27 22:28:18 +0100

    Dialogue System changed to JSON format

--- scripts/ui/dialogue_player.gd
+++ scripts/ui/dialogue_player.gd
@@ -1,13 +1,21 @@
 extends CanvasLayer
 
+@export_file("*json") var scene_text_file : String
 var dialogueDisplaying = false
-var dialogueBox = []
+var dialogueFile = {}
+var playingDialogue = []
 @onready var textBox = $RichTextLabel
 var dialogueIndex = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
+	if FileAccess.file_exists(scene_text_file):
+		var file = FileAccess.open(scene_text_file, FileAccess.READ)
+		var test_json_conv = JSON.new()
+		test_json_conv.parse(file.get_as_text())
+		dialogueFile = test_json_conv.get_data()
+		print(dialogueFile["fish_intro"])
 	SignalBus.connect("display_dialogue", _on_display_dialogue)
 	pass # Replace with function body.
 
@@ -18,16 +26,16 @@ func _process(delta: float) -> void:
 func _on_display_dialogue(dialogue) -> void:
 	if !dialogueDisplaying:
 		visible = true
-		dialogueBox = dialogue
+		playingDialogue = dialogueFile[dialogue]
 		dialogueDisplaying = true
 		dialogueIndex = 0
-		textBox.text = dialogueBox[dialogueIndex]
+		textBox.text = playingDialogue[dialogueIndex]
 	elif dialogueDisplaying:
 		dialogueIndex += 1
-		if dialogueIndex >= dialogueBox.size():
+		if dialogueIndex >= playingDialogue.size():
 			dialogueDisplaying = false
 			visible = false
 			SignalBus.emit_signal("dialogue_done")
 		else:
-			textBox.text = dialogueBox[dialogueIndex]
+			textBox.text = playingDialogue[dialogueIndex]
 	
