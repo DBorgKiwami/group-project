1626c07c295da997f4eca9c6ee7a874696137de0
zhcao-ops <zhcao@tcd.ie>
2026-08-02 18:22:51 +0100

    Make 3D scene transition input reliable

--- scripts/elements/scene_transition.gd
+++ scripts/elements/scene_transition.gd
@@ -4,16 +4,27 @@ extends Area3D
 @export var spawnposition : Vector3
 var active = false
 
+
 func _input(event: InputEvent) -> void:
-	if active and event.is_action_pressed("interact"):
-		print("Hello")
-		if spawnposition:
-			Scenecontroler.load_scene_with_position(scene, spawnposition)
-		else:
-			Scenecontroler.load_scene(scene)
+	if not event.is_action_pressed("interact"):
+		return
+
+	if not active and not has_overlapping_areas():
+		return
+
+	if scene == "":
+		return
+
+	print("Loading scene: " + scene)
+	if spawnposition != Vector3.ZERO:
+		Scenecontroler.load_scene_with_position(scene, spawnposition)
+	else:
+		Scenecontroler.load_scene(scene)
+
 
 func _on_area_entered(area: Area3D) -> void:
 	active = true
+
 
 func _on_area_exited(area: Area3D) -> void:
 	active = false
