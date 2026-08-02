59ced5c4e2eeb240332a97353e38bd04bf0f8506
zhcao-ops <zhcao@tcd.ie>
2026-08-02 18:02:02 +0100

    Use player camera for Redlance playtest

--- scripts/levels/redlance_pondside_review.gd
+++ scripts/levels/redlance_pondside_review.gd
@@ -2,16 +2,22 @@ extends Node3D
 
 @export var add_simple_collision := true
 @export var snap_player_to_spawn := true
+@export var use_preview_camera := false
 
 @onready var level_base: Node3D = $LevelBase
 @onready var preview_camera: Camera3D = $PreviewCamera
 @onready var player: Node3D = $"3Dplatformingcharacter"
+@onready var player_camera: Camera3D = $"3Dplatformingcharacter/Node3D/Camera3D"
 @onready var player_spawn: Marker3D = $GameplayPlan/PlayerSpawn
 
 
 func _ready() -> void:
-	preview_camera.current = true
-	preview_camera.look_at(Vector3(1.6, 0.45, 1.8), Vector3.UP)
+	if use_preview_camera:
+		preview_camera.current = true
+		preview_camera.look_at(Vector3(1.6, 0.45, 1.8), Vector3.UP)
+	else:
+		preview_camera.current = false
+		player_camera.current = true
 
 	if snap_player_to_spawn:
 		player.global_position = player_spawn.global_position
