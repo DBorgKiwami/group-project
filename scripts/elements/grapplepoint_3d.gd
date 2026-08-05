f0126e4c9ecac2f7e64a23c3be558c1c498b19ff
zhcao-ops <zhcao@tcd.ie>
2026-08-05 11:03:34 +0100

    Add grapple prompt and right mouse input

--- scripts/elements/grapplepoint_3d.gd
+++ scripts/elements/grapplepoint_3d.gd
@@ -2,6 +2,7 @@ class_name GrapplePoint3D
 extends Area3D
 
 @export var icon_sprite : Sprite3D
+@export var prompt_label : Label3D
 @export var idle_icon : Texture2D
 @export var grapple_icon : Texture2D
 
@@ -16,20 +17,26 @@ func _ready():
 		normal_scale = icon_sprite.scale
 		icon_sprite.texture = idle_icon
 		start_idle_pulse()
+	if prompt_label:
+		prompt_label.visible = false
 
 
 func highlight():
 	highlighted = true;
-	pass
+	if prompt_label and !is_grappled:
+		prompt_label.visible = true
 
 func unhighlight():
 	highlighted = false;
-	pass
+	if prompt_label:
+		prompt_label.visible = false
 
 
 func start_grapple():
 	is_grappled = true
 	stop_idle_pulse()
+	if prompt_label:
+		prompt_label.visible = false
 	if icon_sprite:
 		icon_sprite.texture = grapple_icon
 		icon_sprite.scale = normal_scale * 1.1
@@ -40,6 +47,8 @@ func end_grapple():
 	if icon_sprite:
 		icon_sprite.texture = idle_icon
 		icon_sprite.scale = normal_scale
+	if prompt_label and highlighted:
+		prompt_label.visible = true
 	start_idle_pulse()
 
 
