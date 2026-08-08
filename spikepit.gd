7efaba8bcf45c652d42ebf1824500fe8e0d4d768
RyanSwingBling <ryan2002liu@gmail.com>
2026-08-08 15:48:14 +0100

    bug fixes

--- spikepit.gd
+++ spikepit.gd
@@ -1,8 +1,9 @@
 extends Area3D
-
 @export var respawnPoint : Node3D
 @export var DEFAULT : Vector3 = Vector3.ZERO
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player3D:
