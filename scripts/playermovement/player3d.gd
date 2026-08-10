7697e3698a3a854b612b05ee7fef2112f1bf17dc
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-08-10 17:39:29 +0100

    Player Camera No Longer Clips through Level

--- scripts/playermovement/player3d.gd
+++ scripts/playermovement/player3d.gd
@@ -11,6 +11,7 @@ class_name Player3D
 
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
+@export var CAMERA_DISTANCE_FROM_GROUND = 1.0
 @export var CAMERA_SPEED = 0.5
 @export var CAMERA_PAN_SPEED = 5.0
 @export var jump_timer_max = 0.2
@@ -129,7 +130,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	camera_pivot.global_position = camera_pivot.global_position.lerp(position, delta * CAMERA_PAN_SPEED)
+	camera_pivot.global_position = camera_pivot.global_position.lerp(position + Vector3(0, CAMERA_DISTANCE_FROM_GROUND, 0), delta * CAMERA_PAN_SPEED)
 	if direction and !inDialogue:
 		lastDirection = direction
 		velocity.x = direction.x * SPEED
