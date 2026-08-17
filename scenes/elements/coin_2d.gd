b0c38cdd95543728256da6c1cc547e0613ac3bde
RyanSwingBling <ryan2002liu@gmail.com>
2026-08-17 20:27:58 +0100

    npcs

--- scenes/elements/coin_2d.gd
+++ scenes/elements/coin_2d.gd
@@ -1,5 +1,4 @@
 extends RigidBody3D
-
 var spawn_impulse : Vector3
 @export var collection_area : Area3D
 @export var collect_sfx : AudioStreamPlayer
@@ -7,21 +6,16 @@ var spawn_impulse : Vector3
 @export var fling_y : float = 10
 var flyout = false
 var _collected := false
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if flyout:
 		apply_impulse(Vector3(randf_range(-fling_x,fling_x),randf_range(0,fling_y),0))
 	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 func _physics_process(delta: float) -> void:
 	pass
-
-
 #Replace this with an animation eventually
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if _collected:
@@ -36,15 +30,3 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 		collect_sfx.play()
 		await collect_sfx.finished
 	queue_free()
-
-func _play_collection_sound() -> void:
-	if collection_sound == null:
-		return
-
-	# The coin disappears immediately, so use a detached player that can finish.
-	var audio_player := AudioStreamPlayer.new()
-	audio_player.stream = collection_sound
-	audio_player.volume_db = -6.0
-	audio_player.finished.connect(audio_player.queue_free)
-	get_tree().root.add_child(audio_player)
-	audio_player.play()
