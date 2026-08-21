b4e357cc50df39c5dd59c45a4e0109834a1d4454
The Majestic yet Deadly Sphinx <113467891+DBorgKiwami@users.noreply.github.com>
2026-08-22 00:45:52 +0100

    shop update

--- scenes/menu/shop_item.gd
+++ scenes/menu/shop_item.gd
@@ -3,14 +3,18 @@ class_name ShopItem
 
 var active = false
 var shop_tag = ""
+var item = ""
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and active:
+			Shop.buy_item_from_shop(shop_tag, item)
+			SignalBus.open_shop.emit(shop_tag)
 			print($ColorRect/RichTextLabel.text)
 
 func load_information(shop, price, name, desc):
 	shop_tag = shop
+	item = name
 	$ColorRect/RichTextLabel.text = str(price) + " " + str(name) + " " + str(desc)
 
 func _on_mouse_entered() -> void:
